123a5a7aa1fcb0a3f9b3a68ddea021c4ba2f9a56
Leonardo Rocha <leonardo.rocha@iacit.com.br>
2026-07-18 14:48:12 -0300

    Corrigindo o basePath do GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/aniversario-ketty",
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
